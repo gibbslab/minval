e16a0f4949a68556fee8013911df4f3c38897244
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-06-08 21:08:04 -0500

    Update MINVAL

--- R/is.validsyntax.R
+++ R/is.validsyntax.R
@@ -7,7 +7,7 @@ is.validsyntax <- function(reaction){
   syntax.validation <- function (reaction){
     if (grepl("([[:digit:]][[:blank:]][[:digit:]][[:blank:]])+",reaction) ||
         grepl("[[:blank:]](<?)-(>?)[[:blank:]]",reaction) ||
-        grepl("[[:alnum:]]\\+[[:alnum:]]*", reaction) ||
+        grepl("[[:alnum:]]+\\+[[:alnum:]]+", reaction) ||
         (!grepl("[[:blank:]]",reaction)) ||
         grepl("[[:blank:]]\\-[[:alnum:]]",reaction) ||
         !grepl("[[:blank:]]<*=>*[[:blank:]]",reaction)){
