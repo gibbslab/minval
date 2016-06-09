bf23c4b0d4d4edc46a652bc02a0a4827a802f067
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-06-08 21:03:15 -0500

    Update is.validsyntax
    New verification step was added

--- R/is.validsyntax.R
+++ R/is.validsyntax.R
@@ -7,9 +7,10 @@ is.validsyntax <- function(reaction){
   syntax.validation <- function (reaction){
     if (grepl("([[:digit:]][[:blank:]][[:digit:]][[:blank:]])+",reaction) ||
         grepl("[[:blank:]](<?)-(>?)[[:blank:]]",reaction) ||
-        grepl("[[:alnum:]]\\+[[:alnum:]]", reaction) ||
+        grepl("[[:alnum:]]\\+[[:alnum:]]*", reaction) ||
         (!grepl("[[:blank:]]",reaction)) ||
-        grepl("[[:blank:]]\\-[[:alnum:]]",reaction)){
+        grepl("[[:blank:]]\\-[[:alnum:]]",reaction) ||
+        !grepl("[[:blank:]]<*=>*[[:blank:]]",reaction)){
       return(FALSE)
     } else{
       return(TRUE)
