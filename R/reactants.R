dce58005bc07c24f0904ebc3bb62b97187e10468
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-06-08 21:05:18 -0500

    Update MINVAL
    Reactant function is now vectorized

--- R/reactants.R
+++ R/reactants.R
@@ -4,17 +4,17 @@
 # Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
 
 reactants <- function(reaction){
-  # Identifies if stoichiometric reaction is not reversible. In this case:
-  if (grepl("<=>",reaction)){
-    reactants <- unlist(strsplit(reaction,"[[:blank:]]*<=>[[:blank:]]*"))
-  } else {
-    reactants <- unlist(strsplit(reaction,"[[:blank:]]*=>[[:blank:]]*"))[1]
-  }
+  reaction <- unlist(lapply(strsplit(as.vector(reaction)," => "),function(reaction){reaction[[1]]}))
+  reaction <- strsplit(reaction, " <=> ")
   # Split independient reactants
-  reactants <- unlist(strsplit(reactants,"[[:blank:]]+\\+[[:blank:]]+"))
-  reactants <- gsub("^[[:blank:]]*","",reactants)
-  reactants <- gsub("[[:blank:]]*$","",reactants)
+  reaction <- lapply(reaction, function(reaction){strsplit(reaction," \\+ ")})
+  # Remove spaces and report uniques
+  reaction <- lapply(reaction, function(reaction){unique(.remove.spaces(unlist(reaction)))})
   # Use a regex to extract stoichiometric coefficients and separate the metabolite name
-  reactants <- .remove_coefficients(reactants)
-  return(reactants[!is.na(reactants)])
+  reactants <- lapply(reaction, function(reaction){unique(.remove_coefficients(reaction))})
+  if (length(reactants)==1){
+    return(unlist(reactants))
+  } else {
+    return(reactants)
+  }
 }
