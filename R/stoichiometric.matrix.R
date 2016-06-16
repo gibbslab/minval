0385fbd8ba7734b27672c8cc9aa40f7325dae5d0
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-06-16 17:14:28 -0500

    Update MINVAL

--- R/stoichiometric.matrix.R
+++ R/stoichiometric.matrix.R
@@ -1,4 +1,5 @@
 stoichiometric.matrix <- function(reactionList){
+  reactionList <- as.vector(reactionList)
   mets<- metabolites(reactionList)
   s <- matrix(0,nrow = length(reactionList),ncol=length(mets),dimnames = list(reactions=paste0("R",formatC(1:length(reactionList),digits = (nchar(length(reactionList))-1),flag = 0)),metabolites=mets))
   for (reaction in 1:length(reactionList)){
@@ -8,8 +9,9 @@ stoichiometric.matrix <- function(reactionList){
     p_coe <- .coefficients(p_met)
     r_met <- metabolites(r_met)
     p_met <- metabolites(p_met)
-    s[reaction,r_met[!r_met%in%p_met]] <- -1*(r_coe[!r_met%in%p_met])
-    s[reaction,p_met[!p_met%in%r_met]] <- p_coe[!p_met%in%r_met]
+    s[reaction,r_met] <- -1*(r_coe)
+    s[reaction,p_met] <- p_coe
+    s[reaction,r_met%in%p_met]<-0
   }
   return(t(s))
 }
