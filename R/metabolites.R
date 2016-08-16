e11b354cb4468b906a9f78e59172bfb1f713c637
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-08-16 16:16:35 -0500

    Update MINVAL
    Metabolites function was added.

---
+++ R/metabolites.R
@@ -0,0 +1,20 @@
+# metabolites
+# Daniel Camilo Osorio
+# Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
+# Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
+
+metabolites <- function(reactionList, woCompartment = FALSE, uniques=TRUE){
+  reactionList <- reactionList[is.validSyntax(reactionList)]
+  reaction <- strsplit(as.vector(reactionList),"[[:blank:]]+<?=>[[:blank:]]*")
+  reaction <- lapply(reaction, function(reaction){strsplit(unlist(reaction),"[[:blank:]]+\\+[[:blank:]]+")})
+  reaction <- lapply(reaction, function(reaction){.remove.spaces(unlist(reaction))})
+  reaction <- lapply(reaction, function(reaction){.remove.coefficients(reaction)})
+  metabolites <- unlist(reaction)
+  if (woCompartment == TRUE) {
+    metabolites <- .remove.compartment(metabolites)
+  }
+  if (uniques == TRUE) {
+    metabolites <- unique(metabolites)
+  }
+  return(metabolites)
+}
