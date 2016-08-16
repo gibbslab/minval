35ccfe423188e733f572ec45b2e209fe45602191
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-08-16 16:39:48 -0500

    Update MINVAL

---
+++ R/products.R
@@ -0,0 +1,28 @@
+# products | Identifies the reactants for a stoichometric reaction
+# Daniel Camilo Osorio
+# Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
+# Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
+
+products <- function(reaction){
+  # Convert to a vector
+  reactionList <- as.vector(reactionList)
+  # Remove reaction with invalid syntax
+  reactionList <- reactionList[is.validSyntax(reactionList)]
+  # Extract reactants for irreversible reactions
+  reaction <- strsplit(reactionList,"[[:blank:]]+=>[[:blank:]]+")
+  reaction[lengths(reaction)>1] <- lapply(reaction[lengths(reaction)>1],function(reaction){reaction[[2]]})
+  # Extract metabolites for reversible reactions
+  reaction <- strsplit(unlist(reaction), "[[:blank:]]+<=>[[:blank:]]+")
+  # Split independient reactants
+  reaction <- lapply(reaction, function(reaction){strsplit(reaction,"[[:blank:]]+\\+[[:blank:]]+")})
+  # Remove spaces and report uniques
+  reaction <- lapply(reaction, function(reaction){unique(.remove.spaces(unlist(reaction)))})
+  # Use a regex to extract stoichiometric coefficients and separate the metabolite name
+  products <- lapply(reaction, function(reaction){unique(.remove.coefficients(reaction))})
+  if (length(products)==1){
+    return(unlist(products))
+  } else {
+    return(products)
+  }
+}
+
