ac237450215cb8df49e4ed7f3392a75af3eddc1f
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-08-16 16:49:06 -0500

    Update MINVAL

---
+++ R/compartments.R
@@ -0,0 +1,19 @@
+# compartments
+# Daniel Camilo Osorio
+# Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
+# Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
+
+compartments <- function(reactionList){
+  # Convert to a vector
+  reactionList <- as.vector(reactionList)
+  # Remove reaction with invalid syntax
+  reactionList <- reactionList[is.validSyntax(reactionList)]
+  # Extract metabolites
+  metabolites <- metabolites(reactionList)
+  # Extract compartments
+  compartments <- unique(unlist(regmatches(metabolites, gregexpr("\\[[[:alnum:]]*(\\_)?[[:alnum:]]*\\]$", metabolites))))
+  # Remove brackets
+  compartments <- gsub("\\[|\\]","",compartments)
+  # Return compartments
+  return(compartments)
+}
