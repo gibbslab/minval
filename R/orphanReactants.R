db607b1175b3942fac75c108d7c208077e78c5fc
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-08-16 16:59:10 -0500

    Update MINVAL

---
+++ R/orphanReactants.R
@@ -0,0 +1,29 @@
+# orphanReactants
+# Daniel Camilo Osorio
+# Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
+# Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
+
+orphanReactants <- function(reactionList, byCompartment=FALSE){
+  # Convert to a vector
+  reactionList <- as.vector(reactionList)
+  # Remove reaction with invalid syntax
+  reactionList <- reactionList[is.validSyntax(reactionList)]
+  # Extract all reactants
+  reactant <- unique(unlist(reactants(reactionList)))
+  # Extract all products
+  product <- unique(unlist(products(reactionList)))
+  # Possible candidates to be introduced into the system by exchange reactions or by adding more internal reactions.
+  orphan <- reactant[!(reactant%in%product)]
+  if (length(orphan)==0){
+    return (NA)
+  } else {
+    if (byCompartment == TRUE){
+      # Return orphans by compartment
+      sapply(compartments(orphan), function(compartment){orphan[grep(paste0("\\[",compartment,"\\]"),orphan)]}, simplify = FALSE)
+    } else {
+      # Return all reactants never produced in any reaction.
+      return(orphan)
+    }
+  }
+}
+
