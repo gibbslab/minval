baaf3b532dc8a05ef469bcf1ce22852abe4d6567
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-08-21 11:27:54 -0500

    Update MINVAL

--- R/getChEBI.R
+++ R/getChEBI.R
@@ -80,7 +80,7 @@ getChEBI <- function(release="latest"){
     message("Creating Formula Associations ... ")
     formula <- formulas[formulas[,"TYPE"]=="FORMULA",c("COMPOUND_ID","CHEMICAL_DATA")]
     colnames(formula) <- c("ID","FORMULA")
-    DB <- unique(merge(DB,formula, by = "ID"))
+    DB <- unique(merge(DB,formula, by = "ID",all.x = TRUE))
     } else { message("NOT AVAILABLE FOR THIS RELEASE")}
   
   message("Downloading molecular weights ... ",appendLF = FALSE)
