49c1314ef8c03dd8f98e2b3260e3f70dd2f0511e
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-08-22 07:57:11 -0500

    Update MINVAL

--- R/orphanProducts.R
+++ R/orphanProducts.R
@@ -7,13 +7,15 @@ orphanProducts <- function(reactionList, byCompartment=FALSE){
   # Convert to a vector
   reactionList <- as.vector(reactionList)
   # Remove reaction with invalid syntax
-  reactionList <- reactionList[is.validSyntax(reactionList)]
+  reactionList <- reactionList[isValidSyntax(reactionList)]
   # Extract all reactants
   reactant <- unique(unlist(reactants(reactionList)))
   # Extract all products
   product <- unique(unlist(products(reactionList)))
   # Possible candidates to be introduced into the system by exchange reactions or by adding more internal reactions.
-  orphan <- product[!(product%in%reactant)]
+  orphan <- rowSums(abs(stoichiometric.matrix(reactionList)))
+  orphan <- c(names(orphan)[orphan<2],product[!(product%in%reactant)])
+  orphan <- unique(orphan[!orphan%in%reactant[!reactant%in%product]])
   if(length(orphan)==0){
     return (NA)
   } else {
