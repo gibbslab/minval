fef184538183e9e1c141473edac6c9ef0eab0864
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-08-31 11:09:13 -0500

    Update MINVAL

--- R/minvalInternal.R
+++ R/minvalInternal.R
@@ -99,7 +99,7 @@
 }
 
 .sbmlCompatible <- function(metabolite){
-  metabolite <- gsub("[[:blank:]]+","",metabolite)
+  metabolite <- gsub("[[:blank:]]+","_",metabolite)
   metabolite <- gsub("[[:punct:]]","_",metabolite)
   return(metabolite)
 }
