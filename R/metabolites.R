9a6d6e57fcc430707946f325254853873ec39726
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-09-06 11:38:43 -0500

    Update MINVAL

--- R/metabolites.R
+++ R/metabolites.R
@@ -4,13 +4,18 @@
 # Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
 #' @title Identify the list of metabolites for a set of stoichiometric reactions
 #' @description This function identifies the list of metabolites for a set of stoichiometric reactions. If \code{'woCompartment'} is \code{'TRUE'} compartment label is removed. If \code{'uniques'} is \code{'TRUE'}, list of uniques is returned.
-#' @param reactionList 
-#' @param woCompartment 
-#' @param uniques 
+#' @param reactionList A set of stoichiometric reaction with the following format: \code{"H2O[c] + Urea-1-carboxylate[c] <=> 2 CO2[c] + 2 NH3[c]"} Where arrows and plus signs are surrounded by a "space character".
+#' It is also expected that stoichiometry coefficients are surrounded by spaces, (nothe the "2" before the CO2[c] or the NH3[c]).
+#' It also expects arrows to be in the form "\code{=>}" or "\code{<=>}". 
+#' Meaning that arrows like "\code{==>}", "\code{<==>}", "\code{-->}" or "\code{->}" will not be parsed and will lead to errors.
+#' @param woCompartment A boolean value \code{'TRUE'} or \code{'FALSE'} to indicate if compartment label should be removed
+#' @param uniques A boolean value \code{'TRUE'} or \code{'FALSE'} to indicate if uniques must be returned
 #' @return A list of metabolites for a set of stoichiometric reactions
 #' @examples 
 #' # Loading data
 #' glycolysis <- read.csv2(system.file("extdata", "glycolysisKEGG.csv", package = "minval"))
+#' 
+#' # Removing stoichiometric reactions without valid syntax
 #' glycolysis <- mapReactions(
 #'                            reactionList = isValidSyntax(glycolysis$REACTION),
 #'                            referenceData = glycolysis,
