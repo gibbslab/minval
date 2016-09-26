965567d076adbaa3b8f3c474d06eff5472bc7b98
Daniel Osorio <dcosorioh@unal.edu.co>
2016-09-26 17:13:48 -0500

    Update MINVAL

--- R/products.R
+++ R/products.R
@@ -1,9 +1,32 @@
 #' @export products
 #' @title Identify the products of a stoichometric reaction
-# Daniel Camilo Osorio
+#' @author Daniel Camilo Osorio <dcosorioh@unal.edu.co>
 # Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
 # Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
+#' @description This function identifies the products for a set of stoichometric reactions.
+#' @param reactionList A set of stoichiometric reaction with the following format: 
+#' 
+#' \code{"H2O[c] + Urea-1-carboxylate[c] <=> 2 CO2[c] + 2 NH3[c]"} 
+#' 
+#' Where arrows and plus signs are surrounded by a "space character".
+#' It is also expected that stoichiometry coefficients are surrounded by spaces, (nothe the "2" before the CO2[c] or the NH3[c]).
+#' It also expects arrows to be in the form "\code{=>}" or "\code{<=>}". 
+#' Meaning that arrows like "\code{==>}", "\code{<==>}", "\code{-->}" or "\code{->}" will not be parsed and will lead to errors.
+#' @return A vector with the identified products in the reaction, or a list if a set of stoichiometric reactions was given.
+#' @examples 
+#' #' # Loading data
+#' glycolysis <- read.csv2(system.file("extdata", "glycolysisKEGG.csv", package = "minval"))
 
+#' # Removing stoichiometric reactions without valid syntax
+#' glycolysis <- mapReactions(
+#' reactionList = isValidSyntax(glycolysis$REACTION),
+#' referenceData = glycolysis,
+#' by = "bool"
+#' )
+#' 
+#' # Extracting products
+#' products(reactionList = "D-Glyceraldehyde 3-phosphate[c] + Orthophosphate[c] + NAD+[c] => 3-Phospho-D-glyceroyl phosphate[c] + NADH[c] + H+[c]")
+#' products(reactionList = glycolysis$REACTION)
 products <- function(reactionList){
   # Convert to a vector
   reactionList <- as.vector(reactionList)
