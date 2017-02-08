8df5ab4e95959fa0e81e60a40b8330dcd8e91a97
Daniel Osorio <dcosorioh@unal.edu.co>
2017-02-08 17:53:08 -0500

    Update MINVAL

---
+++ R/addExchangeReactions.R
@@ -0,0 +1,18 @@
+addExchangeReactions <- function(reconstruction){
+  reconstruction <- as.data.frame.array(reconstruction)
+  if(all(names(reconstruction)%in%c("ID","DESCRIPTION","REACTION","GPR","LOWER.BOUND","UPPER.BOUND","OBJECTIVE"))){
+    orphans <- table(c(orphanReactants(reconstruction[["REACTION"]]),orphanProducts(reconstruction[["REACTION"]])))
+    new <- cbind(sapply(names(orphans), function(metabolite){paste0("EX_",metabolites(metabolite,woCompartment = TRUE),"(",compartments(metabolite),")")},USE.NAMES = FALSE),
+                 sapply(names(orphans), function(metabolite){paste0("Exchange of ", metabolites(metabolite,woCompartment = TRUE))},USE.NAMES = FALSE),
+                 sapply(names(orphans), function(metabolite){paste0(metabolite," <=>")},USE.NAMES = FALSE),
+                 sapply(names(orphans), function(metabolite){""},USE.NAMES = FALSE),
+                 ifelse(names(orphans)%in%orphanReactants(reconstruction[["REACTION"]]),-1000,0),
+                 ifelse(names(orphans)%in%orphanProducts(reconstruction[["REACTION"]]),1000,0),
+                 sapply(names(orphans), function(metabolite){0},USE.NAMES = FALSE))
+    colnames(new) <- colnames(reconstruction)
+    new <- as.data.frame(rbind(reconstruction,new),row.names=NULL)
+    return(new)
+  } else {
+    return(FALSE)
+  }
+}
--- R/compartments.R
+++ R/compartments.R
@@ -3,27 +3,28 @@
 #  Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
 #  Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
 #' @title  Extract the list of unique compartments for the metabolites of a set of stoichiometric reactions.
-#' @description For a given set of stoichiometric reactions, this function identifies the compartments 
+#' @description For a given set of stoichiometric reactions, this function identifies the compartments
 #' associated to each involved metabolite and return a vector with the list of unique compartments identified.
-#' @param reactionList A set of stoichiometric reaction with the following format: 
-#' 
-#' \code{"H2O[c] + Urea-1-carboxylate[c] <=> 2 CO2[c] + 2 NH3[c]"} 
-#' 
+#' @param reactionList A set of stoichiometric reaction with the following format:
+#'
+#' \code{"H2O[c] + Urea-1-carboxylate[c] <=> 2 CO2[c] + 2 NH3[c]"}
+#'
 #' Where arrows and plus signs are surrounded by a "space character".
 #' It is also expected that stoichiometry coefficients are surrounded by spaces, (nothe the "2" before the CO2[c] or the NH3[c]).
-#' It also expects arrows to be in the form "\code{=>}" or "\code{<=>}". 
+#' It also expects arrows to be in the form "\code{=>}" or "\code{<=>}".
 #' Meaning that arrows like "\code{==>}", "\code{<==>}", "\code{-->}" or "\code{->}" will not be parsed and will lead to errors.
+#' @param uniques A boolean value \code{'TRUE/FALSE'} if uniques must be returned
 #' @return A vector with the list of of unique compartments identified for the metabolites of a set of stoichiometric reactions.
 #' @examples
 #' # Using individual reactions
 #' compartments(
 #'  reactionList = "H2O[c] + Urea-1-carboxylate[c] => 2 CO2[c] + 2 NH3[m]"
 #'  )
-#'  
+#'
 #' compartments(
 #'  reactionList = "L-Glutamate[c] <=> CO2[c] + 4-Aminobutanoate[c]"
 #'  )
-#' 
+#'
 #' # From a data.frame
 #' glycolysis <- read.csv2(system.file("extdata", "glycolysisKEGG.csv", package = "minval"))
 #' compartments(
@@ -31,13 +32,22 @@
 #'  )
 #' @keywords Extract Unique Compartments Metabolic Reconstruction
 
-compartments <- function(reactionList){
+compartments <- function(reactionList, uniques = TRUE) {
   # Extract metabolites of a set of stoichiometric reactions
   metabolites <- metabolites(reactionList)
-  # Extract compartments using a regular expression 
-  compartments <- unique(unlist(regmatches(metabolites, gregexpr("\\[[[:alnum:]]*(\\_)?[[:alnum:]]*\\]$", metabolites))))
+  # Extract compartments using a regular expression
+  compartments <-
+    unlist(regmatches(
+      x = metabolites,
+      m =  gregexpr(pattern = "\\[[[:alnum:]]*(\\_)?[[:alnum:]]*\\]$", text = metabolites)
+    ))
   # Remove brackets
-  compartments <- gsub("\\[|\\]","",compartments)
+  compartments <- gsub("\\[|\\]", "", compartments)
   # Return compartments
-  return(compartments)
+  if (uniques == TRUE) {
+    compartments <- unique(compartments)
+    return(compartments)
+  } else {
+    return(compartments)
+  }
 }
