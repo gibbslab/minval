2696cf1d91df398152894db09a63dce03f31598b
Daniel Osorio <dcosorioh@unal.edu.co>
2017-03-12 20:13:45 -0500

    A bug related with balanced metabolites in the stoichiometricMatrix function was fixed

--- R/stoichiometricMatrix.R
+++ R/stoichiometricMatrix.R
@@ -33,7 +33,7 @@ stoichiometricMatrix <- function(reactionList){
   # Remove reaction with invalid syntax
   reactionList <- reactionList[isValidSyntax(reactionList)]
   # Extract metabolites
-  mets<- metabolites(reactionList)
+  mets<- metabolites(reactionList,uniques = TRUE)
   # Create matrix
   s <- matrix(0,nrow = length(reactionList),ncol=length(mets),dimnames = list(reactions=paste0("R",formatC(1:length(reactionList),digits = (nchar(length(reactionList))-1),flag = 0)),metabolites=mets))
   # Fill
@@ -44,6 +44,24 @@ stoichiometricMatrix <- function(reactionList){
     p_coe <- .coefficients(p_met)
     r_met <- metabolites(r_met)
     p_met <- metabolites(p_met)
+    if(any(r_met%in%p_met|p_met%in%r_met)){
+      for(balanced in unique(c(r_met[r_met%in%p_met],p_met[p_met%in%r_met]))){
+        if(r_coe[which(r_met == balanced)] > p_coe[which(p_met == balanced)]) {
+          r_coe[which(r_met == balanced)] <- r_coe[which(r_met == balanced)] - p_coe[which(p_met == balanced)]
+          p_coe <- p_coe[-which(p_met == balanced)]
+          p_met <- p_met[-which(p_met == balanced)]
+        } else if (r_coe[which(r_met == balanced)] < p_coe[which(p_met == balanced)]){
+          p_coe[which(p_met == balanced)] <- p_coe[which(p_met == balanced)] - r_coe[which(r_met == balanced)]
+          r_coe <- r_coe[-which(r_met == balanced)]
+          r_met <- r_met[-which(r_met == balanced)]
+        } else if (r_coe[which(r_met == balanced)] == p_coe[which(p_met == balanced)]){
+          r_coe <- r_coe[-which(r_met == balanced)]
+          p_coe <- p_coe[-which(p_met == balanced)]
+          r_met <- r_met[-which(r_met == balanced)]
+          p_met <- p_met[-which(p_met == balanced)]
+        }
+      }
+    }
     s[reaction,r_met[!is.na(r_met)]] <- -1*(r_coe[!is.na(r_met)])
     s[reaction,p_met[!is.na(p_met)]] <- p_coe[!is.na(p_met)]
     s[reaction,r_met%in%p_met]<-0  
@@ -51,4 +69,3 @@ stoichiometricMatrix <- function(reactionList){
   # Return
   return(t(s))
 }
-
