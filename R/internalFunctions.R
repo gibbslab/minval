d85f4ce2dfa87e3da16288d248da7521165b4c08
Daniel Osorio <dcosorioh@unal.edu.co>
2017-03-18 13:14:19 -0500

    reactionType function was added as internal function

--- R/internalFunctions.R
+++ R/internalFunctions.R
@@ -47,37 +47,72 @@ coefficients <- function(metabolite) {
 
 # Extract metabolites of the left side in a  biochemical reaction
 getLeft <- function(reactionList) {
+  reactionList <- as.vector(reactionList)
   sides <-
     strsplit(x = reactionList, split = "[[:space:]]*<?=>[[:space:]]*")
   metabolites <-
     lapply(sides, function(sides) {
       removeSpaces(unlist(strsplit(x = sides[1], split = "[[:space:]]+\\+[[:space:]]+")))
     })
-  if (length(reactionList) == 1) {
-    metabolites <- unlist(metabolites)
-  }
   return(metabolites)
 }
 
 # Extract metabolites of the right side in a  biochemical reaction
 getRight <- function(reactionList) {
+  reactionList <- as.vector(reactionList)
   sides <-
     strsplit(x = reactionList, split = "[[:space:]]*<?=>[[:space:]]*")
   metabolites <-
     lapply(sides, function(sides) {
       removeSpaces(unlist(strsplit(x = sides[2], split = "[[:space:]]+\\+[[:space:]]+")))
     })
-  if (length(reactionList) == 1) {
-    metabolites <- unlist(metabolites)
-  }
   return(metabolites)
 }
 
 # Split chemical formula
 splitFormula <- function(chemicalFormula) {
-  splitAtoms <- unlist(regmatches(formula, gregexpr("([A-Z]{1}[a-z]?)([0-9]*)", chemicalFormula)))
+  splitAtoms <-
+    unlist(regmatches(
+      formula,
+      gregexpr("([A-Z]{1}[a-z]?)([0-9]*)", chemicalFormula)
+    ))
   atoms <- sub("([A-Z]{1}[a-z]?)([0-9]*)", '\\1', splitAtoms)
-  atomsNumber <- as.numeric(regmatches(x = splitAtoms, m = gregexpr('[0-9]+', splitAtoms)))
+  atomsNumber <-
+    as.numeric(regmatches(x = splitAtoms, m = gregexpr('[0-9]+', splitAtoms)))
   atomsNumber[is.na(atomsNumber)] <- 1
   tapply(atomsNumber, atoms, sum)
+}
+
+# Identify the reaction type
+reactionType <- function(reactionList) {
+  # Convert to a vector
+  reactionList <- as.vector(reactionList)
+  # Validate syntax
+  reactionList <-
+    reactionList[validateSyntax(reactionList = reactionList)]
+  # Split reaction
+  left <- getLeft(reactionList)
+  right <- getRight(reactionList)
+  left <-
+    lapply(left, function(left) {
+      compartments(reactionList = unlist(left))
+    })
+  right <-
+    lapply(right, function(right) {
+      compartments(reactionList = unlist(right))
+    })
+  # Define type of reaction
+  sapply(seq_along(reactionList), function(reaction) {
+    if (length(right[[reaction]]) > 0) {
+      if (length(left[[reaction]]) > 1 | length(right[[reaction]]) > 1) {
+        return("Transport reaction")
+      } else if (all.equal(target = left[[reaction]], current = right[[reaction]]) == TRUE) {
+        return("Compartmentalized reaction")
+      } else {
+        return("Transport reaction")
+      }
+    } else {
+      return ("Exchange reaction")
+    }
+  })
 }
