8bef08d648bd4b641314cda7c21c73e0eba43288
Daniel Osorio <dcosorioh@unal.edu.co>
2017-03-19 16:06:24 -0500

    writeTSV function dependencies were added

--- R/writeTSV.R
+++ R/writeTSV.R
@@ -1,4 +1,5 @@
 #' @export writeTSV
+#' @importFrom utils write.table
 #' @author Daniel Camilo Osorio <dcosorioh@unal.edu.co>
 #  Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
 #  Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
