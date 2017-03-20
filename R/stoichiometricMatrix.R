3053cd73836145bb062cfc96a4e3003d029fbf79
Daniel Osorio <dcosorioh@unal.edu.co>
2017-03-20 11:12:42 -0500

    stoichiometricMatrix function documentation and examples were added

--- R/stoichiometricMatrix.R
+++ R/stoichiometricMatrix.R
@@ -3,16 +3,30 @@
 #' @author Daniel Camilo Osorio <dcosorioh@unal.edu.co>
 # Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
 # Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
-#' @description  A set of stoichiometric reactions are often represented in a more compact form called the stoichiometry matrix. If a reaction network has n reactions and m participating metabolites then the stoichiometry matrix will have correspondingly m rows and n columns.
-#' @param reactionList A set of stoichiometric reaction with the following format: 
-#' 
-#' \code{"H2O[c] + Urea-1-carboxylate[c] <=> 2 CO2[c] + 2 NH3[c]"} 
-#' 
-#' Where arrows and plus signs are surrounded by a "space character".
-#' It is also expected that stoichiometry coefficients are surrounded by spaces, (nothe the "2" before the CO2[c] or the NH3[c]).
-#' It also expects arrows to be in the form "\code{=>}" or "\code{<=>}". 
-#' Meaning that arrows like "\code{==>}", "\code{<==>}", "\code{-->}" or "\code{->}" will not be parsed and will lead to errors.
+#' @description  A set of stoichiometric reactions are often represented in a more compact form called the stoichiometry matrix. 
+#' If a metabolic network has n reactions and m participating metabolites then the stoichiometry matrix will have correspondingly m rows and n columns. 
+#' Values in stoichiometric matrix represent the metabolite coefficients in each reaction.
+#' @param reactionList A set of stoichiometric reaction with the following characteristics: \itemize{
+#' \item Arrows symbols must be given in the form \code{'=>'} or \code{'<=>'}
+#' \item Inverse arrow symbols \code{'<='} or other types as: \code{'-->'}, \code{'<==>'}, \code{'->'} will not be parsed and will lead to errors.
+#' \item Arrow symbols and plus signs (\code{+}) must be surrounded by a space character
+#' \item Stoichiometric coefficients must be surrounded by a space character and not by parentheses.
+#' \item Each metabolite must have only one stoichiometric coefficient, substituents must be joined to metabolite name by a hyphen (\code{-}) symbol.
+#' \item Exchange reactions have only one metabolite before arrow symbol
+#' \item Compartments must be given between square brackets ([compartment]) joined at the end of metabolite name
+#' }
+#' Some examples of valid stoichiometric reactions are: \itemize{
+#' \item \code{H2O[c] + Urea-1-Carboxylate[c] <=> 2 CO2[c] + 2 NH3[c]}
+#' \item \code{ADP[c] + Phosphoenolpyruvate[c] => ATP[c] + Pyruvate[c]}
+#' \item \code{CO2[c] <=> }
+#' }
 #' @return The stoichiometric matrix for a given set of stoichiometric reactions
+#' @examples 
+#' # Loading a set of stoichiometric reactions
+#' glycolysis <- read.csv(system.file("extdata/glycolysisModel.csv",package = "minval"), sep='\t')
+#'
+#' #Building the stoichiometric matrix
+#' stoichiometricMatrix(reactionList = glycolysis$REACTION)
 
 stoichiometricMatrix <- function(reactionList){
   # Convert to a vector
