9776d5d39e2bb8e81f5b17873befbfe5d01270d3
Daniel Osorio <dcosorioh@unal.edu.co>
2017-03-20 11:15:17 -0500

    stoichiometricMatrix function code was fixed

--- R/stoichiometricMatrix.R
+++ R/stoichiometricMatrix.R
@@ -3,8 +3,8 @@
 #' @author Daniel Camilo Osorio <dcosorioh@unal.edu.co>
 # Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
 # Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
-#' @description  A set of stoichiometric reactions are often represented in a more compact form called the stoichiometry matrix. 
-#' If a metabolic network has n reactions and m participating metabolites then the stoichiometry matrix will have correspondingly m rows and n columns. 
+#' @description  A set of stoichiometric reactions are often represented in a more compact form called the stoichiometry matrix.
+#' If a metabolic network has n reactions and m participating metabolites then the stoichiometry matrix will have correspondingly m rows and n columns.
 #' Values in stoichiometric matrix represent the metabolite coefficients in each reaction.
 #' @param reactionList A set of stoichiometric reaction with the following characteristics: \itemize{
 #' \item Arrows symbols must be given in the form \code{'=>'} or \code{'<=>'}
@@ -21,41 +21,56 @@
 #' \item \code{CO2[c] <=> }
 #' }
 #' @return The stoichiometric matrix for a given set of stoichiometric reactions
-#' @examples 
+#' @examples
 #' # Loading a set of stoichiometric reactions
 #' glycolysis <- read.csv(system.file("extdata/glycolysisModel.csv",package = "minval"), sep='\t')
 #'
 #' #Building the stoichiometric matrix
 #' stoichiometricMatrix(reactionList = glycolysis$REACTION)
 
-stoichiometricMatrix <- function(reactionList){
+stoichiometricMatrix <- function(reactionList) {
   # Convert to a vector
   reactionList <- as.vector(reactionList)
   # Remove reaction with invalid syntax
   reactionList <- reactionList[validateSyntax(reactionList)]
   # Extract metabolites
-  mets<- metabolites(reactionList,uniques = TRUE)
+  mets <- metabolites(reactionList, uniques = TRUE)
   # Create matrix
-  s <- matrix(0,nrow = length(reactionList),ncol=length(mets),dimnames = list(reactions=paste0("R",formatC(1:length(reactionList),digits = (nchar(length(reactionList))-1),flag = 0)),metabolites=mets))
+  s <-
+    matrix(
+      0,
+      nrow = length(reactionList),
+      ncol = length(mets),
+      dimnames = list(
+        reactions = paste0("R", formatC(
+          1:length(reactionList),
+          digits = (nchar(length(reactionList)) - 1),
+          flag = 0
+        )),
+        metabolites = mets
+      )
+    )
   # Fill
-  for (reaction in seq_along(reactionList)){
+  for (reaction in seq_along(reactionList)) {
     r_met <- unlist(getLeft(reactionList[reaction]))
     r_coe <- coefficients(r_met)
     p_met <- unlist(getRight(reactionList[reaction]))
     p_coe <- coefficients(p_met)
     r_met <- metabolites(r_met)
     p_met <- metabolites(p_met)
-    if(any(r_met%in%p_met|p_met%in%r_met)){
-      for(balanced in unique(c(r_met[r_met%in%p_met],p_met[p_met%in%r_met]))){
-        if(r_coe[which(r_met == balanced)] > p_coe[which(p_met == balanced)]) {
-          r_coe[which(r_met == balanced)] <- r_coe[which(r_met == balanced)] - p_coe[which(p_met == balanced)]
+    if (any(r_met %in% p_met | p_met %in% r_met)) {
+      for (balanced in unique(c(r_met[r_met %in% p_met], p_met[p_met %in% r_met]))) {
+        if (r_coe[which(r_met == balanced)] > p_coe[which(p_met == balanced)]) {
+          r_coe[which(r_met == balanced)] <-
+            r_coe[which(r_met == balanced)] - p_coe[which(p_met == balanced)]
           p_coe <- p_coe[-which(p_met == balanced)]
           p_met <- p_met[-which(p_met == balanced)]
-        } else if (r_coe[which(r_met == balanced)] < p_coe[which(p_met == balanced)]){
-          p_coe[which(p_met == balanced)] <- p_coe[which(p_met == balanced)] - r_coe[which(r_met == balanced)]
+        } else if (r_coe[which(r_met == balanced)] < p_coe[which(p_met == balanced)]) {
+          p_coe[which(p_met == balanced)] <-
+            p_coe[which(p_met == balanced)] - r_coe[which(r_met == balanced)]
           r_coe <- r_coe[-which(r_met == balanced)]
           r_met <- r_met[-which(r_met == balanced)]
-        } else if (r_coe[which(r_met == balanced)] == p_coe[which(p_met == balanced)]){
+        } else if (r_coe[which(r_met == balanced)] == p_coe[which(p_met == balanced)]) {
           r_coe <- r_coe[-which(r_met == balanced)]
           p_coe <- p_coe[-which(p_met == balanced)]
           r_met <- r_met[-which(r_met == balanced)]
@@ -63,9 +78,9 @@ stoichiometricMatrix <- function(reactionList){
         }
       }
     }
-    s[reaction,r_met[!is.na(r_met)]] <- -1*(r_coe[!is.na(r_met)])
-    s[reaction,p_met[!is.na(p_met)]] <- p_coe[!is.na(p_met)]
-    s[reaction,r_met%in%p_met]<-0  
+    s[reaction, r_met[!is.na(r_met)]] <- -1 * (r_coe[!is.na(r_met)])
+    s[reaction, p_met[!is.na(p_met)]] <- p_coe[!is.na(p_met)]
+    s[reaction, r_met %in% p_met] <- 0
   }
   # Return
   return(t(s))
