5857af76408ab23fd570c3921449d3ff5bd8bfca
Daniel Osorio <dcosorioh@unal.edu.co>
2017-04-01 09:43:50 -0500

    characterizeReactions function was added and fully documented

--- R/characterizeReactions.R
+++ R/characterizeReactions.R
@@ -1,9 +1,15 @@
-#' @export characterizeRXNs
+#' @export characterizeReactions
 #' @author Daniel Camilo Osorio <dcosorioh@unal.edu.co>
 #  Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
 #  Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
 #' @title Characterize stoichiometric reactions by compartments and reaction type
-#' @description For a given set of stoichiometric reactions, this function: \itemize{\item Counts the number of reactions, \item Compute the relative frequency of each reaction type (transport, exchange and compartmentalized), \item Compute the relative frequency of reactions by compartment, \item Count the number of unique metabolites, \item Compute the relative frequency of metabolites by compartment.}
+#' @description For a given set of stoichiometric reactions, this function: \itemize{
+#' \item Counts the number of reactions, 
+#' \item Computes the relative frequency of each reaction type (transport, exchange and compartmentalized), 
+#' \item Computes the relative frequency of reactions by compartment, 
+#' \item Counts the number of unique metabolites, 
+#' \item Computes the relative frequency of metabolites by compartment.
+#' }
 #' @param reactionList A set of stoichiometric reaction with the following characteristics: \itemize{
 #' \item Arrows symbols must be given in the form \code{'=>'} or \code{'<=>'}
 #' \item Inverse arrow symbols \code{'<='} or other types as: \code{'-->'}, \code{'<==>'}, \code{'->'} will not be parsed and will lead to errors.
@@ -19,31 +25,39 @@
 #' \item \code{CO2[c] <=> }
 #' }
 #' @param rawOutput A boolean value \code{'TRUE'} or \code{'FALSE'} if computed values should be returned instead of raw data
-#' @examples 
+#' @examples
 #' # Loading a set of stoichiometric reactions
 #' glycolysis <- read.csv(system.file("extdata/glycolysisModel.csv",package = "minval"), sep='\t')
 #'
 #' # Characterizing the reactions
-#' characterizeRXNs(reactionList = glycolysis$REACTION)
-characterizeRXNs <- function (reactionList, rawOutput = FALSE){
-  reactionList <- as.vector(reactionList)[validateSyntax(reactionList = reactionList)]
+#' characterizeReactions(reactionList = glycolysis$REACTION)
+characterizeReactions <- function (reactionList, rawOutput = FALSE) {
+  reactionList <-
+    as.vector(reactionList)[validateSyntax(reactionList = reactionList)]
   model <- list ()
   model$nReactions <- length(unique(reactionList))
   model$rType <- reactionType(reactionList = reactionList)
-  model$cReaction <- unlist(sapply(reactionList[model$rType == "Compartmentalized reaction"], function(reaction){compartments(reactionList = reaction, uniques = TRUE)},USE.NAMES = FALSE))
+  model$cReaction <-
+    unlist(sapply(reactionList[model$rType == "Compartmentalized reaction"], function(reaction) {
+      compartments(reactionList = reaction, uniques = TRUE)
+    }, USE.NAMES = FALSE))
   model$rCompartments <- compartments(reactionList = reactionList)
-  model$nMetabolites <- metabolites(reactionList = reactionList, uniques = TRUE)
-  model$cMetabolites <- compartments(reactionList = model$nMetabolites, uniques = FALSE)
+  model$nMetabolites <-
+    metabolites(reactionList = reactionList, uniques = TRUE)
+  model$cMetabolites <-
+    compartments(reactionList = model$nMetabolites, uniques = FALSE)
   model$nMetabolites <- length(model$nMetabolites)
-  if (rawOutput == TRUE){
+  if (rawOutput == TRUE) {
     return(model)
   } else {
     summary <- list ()
     summary$nReactions <- model$nReactions
     summary$rType <- (table(model$rType) / model$nReactions) * 100
-    summary$cReaction <- (table(model$cReaction)/ model$nReactions) * 100
+    summary$cReaction <-
+      (table(model$cReaction) / model$nReactions) * 100
     summary$nMetabolites <- model$nMetabolites
-    summary$cMetabolites <- (table(model$cMetabolites)/ model$nMetabolites) * 100
+    summary$cMetabolites <-
+      (table(model$cMetabolites) / model$nMetabolites) * 100
     return(summary)
   }
 }
